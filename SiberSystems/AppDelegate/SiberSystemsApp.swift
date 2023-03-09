1ad9e12b644c5ba4df9170e8caf103f298779ac2
SigayAleksey <sigayaleksey@gmail.com>
2023-03-09 11:17:06 +0100

    first commit

---
+++ SiberSystems/AppDelegate/SiberSystemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiberSystemsApp.swift
+//  SiberSystems
+//
+//  Created by Alexey Sigay on 09.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SiberSystemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
